98dfe91729d4f757fd8d19a3363180fc53aa302c
nois0720 <dbtjr0720@gmail.com>
2016-11-27 17:25:43 +0900

    modify views folder path, remove unnecessary files

--- wsdproj/app.js
+++ wsdproj/app.js
@@ -12,7 +12,7 @@ var test2 = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'client/views'));
 app.set('view engine', 'ejs');
 //app.use(express.static(path.join(__dirname, 'client')));
 
