16c1ada00ee0f6ea52bd28a9d5a03746856701d9
nois0720 <dbtjr0720@gmail.com>
2016-12-01 13:02:15 +0900

    Remove Angular2 module and reset project

--- wsdproj/client/app/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by Nois on 2016. 11. 22..
- */
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
