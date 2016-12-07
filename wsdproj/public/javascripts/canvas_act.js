bbf613bf85b773e3494f7f9816c1f63f02bab722
nois0720 <dbtjr0720@gmail.com>
2016-12-07 23:51:49 +0900

    remove login project

--- wsdproj/public/javascripts/canvas_act.js
+++ wsdproj/public/javascripts/canvas_act.js
@@ -15,11 +15,10 @@ var PC_posX_cell = 3;
 var PC_posY_cell = 4;
 var PC_posX = cellWidth * PC_posX_cell;
 var PC_posY = cellHeight * PC_posY_cell;
-var PC_dir = 0; //up:0 , right:1, down:2, left:3
 
 level_img.onload = function () {
     context.drawImage(level_img, 0, 0, 256, 256);
 }
 PC_img.onload = function () {
-    context.drawImage(PC_img, PC_posX, PC_posY, 32, 32);
+    context.drawImage(PC_img, PC_posX, PC_posY, cellWidth, cellHeight);
 }
