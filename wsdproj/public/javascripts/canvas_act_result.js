62c3128a852516bdc2b448378c54291f32f923a6
nois0720 <dbtjr0720@gmail.com>
2016-12-09 14:43:26 +0900

    modify interval logic

--- wsdproj/public/javascripts/canvas_act_result.js
+++ wsdproj/public/javascripts/canvas_act_result.js
@@ -1,13 +1,11 @@
 /**
  * Created by Nois on 2016. 12. 2..
  */
-var gameObjectID = {
-    none: 0,
-    pc: 1,
-    obstacle: 2,
-    goal: 3
-};
-var temp;
+var cmdCode = {
+    move: '1',
+    turnLeft: '2'
+}
+
 var level = [
     [0, 0, 2, 0, 0, 0, 0, 0],
     [0, 0, 1, 0, 0, 0, 0, 0],
@@ -31,11 +29,13 @@ var mapHeight = 256;
 var cellWidth = 32;
 var cellHeight = 32;
 
-var PC_posX_cell = 3;
-var PC_posY_cell = 4;
-var PC_posX = cellWidth * PC_posX_cell;
-var PC_posY = cellHeight * PC_posY_cell;
+var PC_posX_cell;
+var PC_posY_cell;
+var PC_posX = 0;
+var PC_posY = 0;
 var obstaclePosArr = new Array(); //obstacle position
+var PC_angle = 0;
+var PC_dir = Dir.up;
 
 var parseLevel = function () {
     for (var i = 0; i < 8; i++) {
@@ -52,37 +52,52 @@ var parseLevel = function () {
             }
         }
     }
+    PC_posX = cellWidth * PC_posX_cell;
+
+    PC_posY = cellHeight * PC_posY_cell;
 };
 
-parseLevel();
-
-PC_posX = cellWidth * PC_posX_cell;
-PC_posY = cellHeight * PC_posY_cell;
-
-
-var PC_dir = Dir.up;
-var PC_angle = 0;
-
 var intervalId;
+var intervalList = [];
+
 var commandList = document.getElementsByClassName('commandList');
 
-(function () {
+function startLogic() {
     for (var i = 0, max = commandList.length; i < max; i++) {
         (function (cmdIndex) {
             setTimeout(function () {
-                cmdProcess(cmdIndex);
+                startCmdProcess(cmdIndex);
             }, 1000 * cmdIndex);
+
+            setTimeout(function () {
+                stopCmdProcess(cmdIndex);
+            }, 1000 * (cmdIndex + 1));
         })(i);
     }
+};
+
+(function init() {
+    parseLevel();
+    startLogic();
 })();
 
-function cmdProcess(index) {
+function startCmdProcess(index) {
     if (commandList[index].innerHTML == '1') {
         intervalId = setInterval(move, (1000 / 32));
-        console.log("set interval " + intervalId);
     } else if (commandList[index].innerHTML == '2') {
         intervalId = setInterval(rotate, (1000 / 90));
-        console.log("set interval " + intervalId);
+    }
+
+    intervalList.push(intervalId);
+}
+
+function stopCmdProcess(index) {
+    clearInterval(intervalList[index]);
+    if (commandList[index].innerHTML == cmdCode.move) {
+        moveCount = 0;
+    } else if (commandList[index].innerHTML == cmdCode.turnLeft) {
+        rotateCount = 0;
+        PC_dir = (PC_dir + 1) % 4;
     }
 }
 
@@ -96,12 +111,6 @@ function move() {
     if (PC_dir == Dir.up)  PC_posY--;
     if (PC_dir == Dir.down)  PC_posY++;
 
-    if (moveCount == cellWidth - 1) {
-        moveCount = 0;
-        clearInterval(intervalId);
-        console.log("clear interval " + intervalId);
-    }
-
     draw();
 }
 
@@ -113,15 +122,11 @@ function rotate() {
     if (rotateCount < 90) {
         PC_angle++;
         PC_angle %= 360;
-    } else {
-        clearInterval(intervalId);
-        rotateCount = 0;
-        PC_dir = (PC_dir + 1) % 4;
-        console.log("clear interval " + intervalId);
     }
 
     draw();
 }
+
 
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
@@ -134,14 +139,6 @@ level_img.src = "/images/testLevel_jpeg.jpg";
 PC_img.src = "/images/PC.png";
 obstacle_img.src = "/images/obstacle.png";
 
- //level_img.onload = function () {
- //    context.drawImage(level_img, 0, 0, mapWidth, mapHeight);
- //}
- //PC_img.onload = function () {
- //    context.drawImage(PC_img, PC_posX, PC_posY, cellWidth, cellHeight);
- //}
-
-
 function draw() {
     // Clear Canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -149,7 +146,7 @@ function draw() {
     // Draw 8x8 Table
     context.drawImage(level_img, 0, 0, mapWidth, mapHeight);
     for (var i = 0; i < obstaclePosArr.length; i++) {
-        context.drawImage(obstacle_img, obstaclePosArr[i].obs_posX * cellWidth+1.5, obstaclePosArr[i].obs_posY * cellHeight+1, 28, 28);
+        context.drawImage(obstacle_img, obstaclePosArr[i].obs_posX * cellWidth + 1.5, obstaclePosArr[i].obs_posY * cellHeight + 1, 28, 28);
     }
     // Draw Main Character
     context.save();
