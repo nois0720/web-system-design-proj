d1035432bb0139eeb3a5c165e57d5e8237fabace
nois0720 <dbtjr0720@gmail.com>
2016-12-11 00:09:28 +0900

    add logout

--- wsdproj/app_server/routes/logout.js
+++ wsdproj/app_server/routes/logout.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/logout', function(req, res){
+router.post('/', function (req, res) {
     res.clearCookie('user');
     res.clearCookie('pass');
-    req.session.destroy(function(e){ res.status(200).send('ok'); });
+    req.session.destroy(function (e) {
+        res.redirect('/');
+    });
 })
 
 module.exports = router;
