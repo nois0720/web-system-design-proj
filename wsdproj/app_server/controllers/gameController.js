a60882d1605e10d7cd6ba2d6f64ae67a7d11a7e5
daeyoung <ndy766@naver.com>
2016-12-10 15:46:06 +0900

    level information send To result.ejs From game.ejs

--- wsdproj/app_server/controllers/gameController.js
+++ wsdproj/app_server/controllers/gameController.js
@@ -11,7 +11,6 @@ var Level = require('../models/level');
 module.exports.startGame = function (req, res) {
 
     var createTime = req.body.createTime;
-    console.log(createTime);
 
     var level = new Level();
 
@@ -25,10 +24,33 @@ module.exports.startGame = function (req, res) {
         }
         else {
             //console.log(obj.levelTable);
-            res.render('game', {title: 'Express', level: obj.levelTable});
+            res.render('game', {title: 'Express', level: obj.levelTable, createTime:obj.createTime});
         }
     });
 };
+
+//send command to result page
+module.exports.gameResult = function(req, res){
+
+    var sample_arr = req.body.test_array;
+    var createTime = req.body.createTime;
+
+    Level.findOne({createTime:createTime }, function (err, obj) {
+        if (err) {
+            console.log('디비에러');
+            res.render('error');
+        }
+        else if(obj == null){
+            res.render('error');
+        }
+        else {
+            console.log(obj.levelTable);
+            res.render('result', {arr: sample_arr, levelTable:obj.levelTable});
+        }
+    });
+
+};
+
 
 module.exports.getLevelList = function(req, res){
     var level = new Level();
